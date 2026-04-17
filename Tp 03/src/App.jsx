76525dece51c2aea2a5ab3c1c71ee5588189810d
IGNACIO ESKENAZI <49374119@ort.edu.ar>
2026-04-17 09:03:59 -0300

    Avance App.jsx y Formulario.jsx

--- Tp 03/src/App.jsx
+++ Tp 03/src/App.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import Header from "./Componentes/Header";
 import Formulario from "./Componentes/Formulario";
 import ListaCita from "./Componentes/ListaCita";
 
 function App() {
+  const [citas, setCitas] = useState([]);
+
   return (
     <>
       <Header />
       <div className="container">
         <div className="row">
-          <Formulario />
-          <ListaCita />
+          <Formulario setCitas={setCitas} />
+          <ListaCita citas={citas} setCitas={setCitas} />
         </div>
       </div>
     </>
--- Tp 03/src/Componentes/Formulario.jsx
+++ Tp 03/src/Componentes/Formulario.jsx
@@ -1,19 +1,49 @@
+import { useState } from "react";
 import "./Formulario.css";
 import Boton from "./Boton";
 import Input from "./Input";
 import Titulo from "./Titulo.jsx";
 
-function Formulario() {
+function Formulario({ setCitas }) {
+  const [form, setForm] = useState({
+    mascota: "",
+    dueno: "",
+    fecha: "",
+    hora: "",
+    sintomas: ""
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setCitas((prev) => [...prev, form]);
+
+    setForm({
+      mascota: "",
+      dueno: "",
+      fecha: "",
+      hora: "",
+      sintomas: ""
+    });
+  };
+
   return (
     <div className="one-half column">
       <Titulo texto="Crear mi Cita" tipo="h2" />
 
-      <form>
-        <Input label="Nombre Mascota" name="mascota" />
-        <Input label="Nombre Dueño" name="dueno" placeholder="Nombre dueño de la mascota" />
-        <Input label="Fecha" name="fecha" tipo="date" />
-        <Input label="hora" name="hora" tipo="time" />
-        <Input label="Sintomas" name="sintomas" tipo="textarea" placeholder="" />
+      <form onSubmit={handleSubmit}>
+        <Input label="Nombre Mascota" name="mascota" value={form.mascota} onChange={handleChange} />
+        <Input label="Nombre Dueño" name="dueno" value={form.dueno} onChange={handleChange} />
+        <Input label="Fecha" name="fecha" tipo="date" value={form.fecha} onChange={handleChange} />
+        <Input label="Hora" name="hora" tipo="time" value={form.hora} onChange={handleChange} />
+        <Input label="Sintomas" name="sintomas" tipo="textarea" value={form.sintomas} onChange={handleChange} />
 
         <Boton texto="Agregar Cita" tipo="submit" clase="u-full-width button-primary" />
       </form>
@@ -22,4 +52,3 @@ function Formulario() {
 }
 
 export default Formulario;
-
